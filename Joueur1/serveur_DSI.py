95e068b24a1a0d8e3f5f2d158ae2b44480d2416c
OSKAR MOREL <oskar.morel@iut.local>
2022-11-15 08:57:38 +0100

    Changement du code en méthodes, tout est en anglais. TODO[Tests automatisés, le .exe et tests sur deux machines différentes

--- Joueur1/serveur_DSI.py
+++ Joueur1/serveur_DSI.py
@@ -11,87 +11,96 @@ with open(path + '\QA.json') as QA:
     data = json.load(QA)
 
 # Création de la socket du serveur
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Description socket : ', s)
-except OSError:
-    print('Création socket échouée')
-else:
-    print('Création socket réussie')
-    coord_S = ('127.0.0.1', 65432)
-    # Association des coordonnées à la socket créée 
+def creationSocket():
     try:
-        s.bind(coord_S)
-        s.listen(1)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except OSError:
-        print('bind() échoué')
-        s.close()
+        print('Socket creation failed')
     else:
-        print('bind() réussi')
+        print('Socket creation success')
+        coord_S = ('127.0.0.1', 65432)
+        # Association des coordonnées à la socket créée 
+        try:
+            s.bind(coord_S)
+            s.listen(1)
+        except OSError:
+            print('bind() fail')
+            s.close()
+        else:
+            print('bind() success')
+    return s
 
 # Acceptation de la connexion avec le client (l'expert en cybersécurité)
-(s_comm, coord_C) = s.accept()
-print("Connection etablie\n")
+def accepter(s):
+    (s_comm, coord_C) = s.accept()
+    print("connection established\n")
+    return(s_comm)
 
-ok = True
-premier = False
-choixAFaire = True
 
-# Explication du contexte
-print("You are a CIO, your company has been hacked. That's why you called a cybersecurity expert.\nYou will have to answer his questions so that he can help you.\nGood luck.\n")
 
-while ok :
-    question2 = True
-    receptionClient = s_comm.recv(1024).decode()
-    print("reception du client : ", receptionClient)
+def programmePrincipal(s, s_comm):
+    ok = True
+    premier = False
+    choixAFaire = True
+    # Explication du contexte
+    print("You are a CIO, your company has been hacked. That's why you called a cybersecurity expert.\nYou will have to answer his questions so that he can help you.\nGood luck.\n")
+    while ok :
+        question2 = True
+        receptionClient = s_comm.recv(1024).decode()
 
-    if receptionClient != "0":
-        if premier:
-            if data[cleAEnvoyer]["question1"]["cleNext"] == receptionClient:
-                print(data[cleAEnvoyer]["question1"]["question"])
+        if receptionClient != "0":
+            if premier:
+                if data[cleAEnvoyer]["question1"]["cleNext"] == receptionClient:
+                    print(data[cleAEnvoyer]["question1"]["question"]+ "\n")
+                else:
+                    print(data[cleAEnvoyer]["question2"]["question"] + "\n")
             else:
-                print(data[cleAEnvoyer]["question2"]["question"])
+                print(data["1"]["question"])
+
+            # Choix de la réponse à envoyer au client (l'expert en cybersécurité)
+            print("1) " + data[receptionClient]["reponse1"]["reponse"])
+            try:
+                print("2) " + data[receptionClient]["reponse2"]["reponse"] + "\n")
+            except:
+                print("")
+                question2 = False
+
+            choix = input("Choose between 1 and 2 : ")
+            if choix == "1":
+                cleAEnvoyer = data[receptionClient]["reponse1"]["cleNext"]
+            elif choix == "2" and question2 == True:
+                cleAEnvoyer = data[receptionClient]["reponse2"]["cleNext"]
+            else:
+                while(choix != "1" and choix !="2" or question2 == False):
+                        choix = input("Choose between 1 and 2 : ")       
+                        if choix == "1":
+                            cleAEnvoyer = data[receptionClient]["reponse1"]["cleNext"]
+                            question2 = True
+                        elif choix == "2" and question2 == True:
+                            cleAEnvoyer = data[receptionClient]["reponse2"]["cleNext"]
+
+            premier = True
+            # Arret de la boucle si arriver a la fin de l'arbre
+            if cleAEnvoyer == "0":
+                print("\nThis is the end of the game\n")
+                ok = False
+            s_comm.send(cleAEnvoyer.encode())
+
         else:
-            print(data["1"]["question"])
-
-        # Choix de la réponse à envoyer au client (l'expert en cybersécurité)
-        print("1) " + data[receptionClient]["reponse1"]["reponse"])
-        try:
-            print("2) " + data[receptionClient]["reponse2"]["reponse"])
-        except:
-            print("")
-            question2 = False
-
-        choix = input("Choose between 1 and 2 : ")
-        if choix == "1":
-            cleAEnvoyer = data[receptionClient]["reponse1"]["cleNext"]
-        elif choix == "2" and question2 == True:
-            cleAEnvoyer = data[receptionClient]["reponse2"]["cleNext"]
-        else:
-            while(choix != "1" and choix !="2" or question2 == False):
-                    choix = input("Choose between 1 and 2 : ")       
-                    if choix == "1":
-                        cleAEnvoyer = data[receptionClient]["reponse1"]["cleNext"]
-                        question2 = True
-                    elif choix == "2" and question2 == True:
-                        cleAEnvoyer = data[receptionClient]["reponse2"]["cleNext"]
-
-        premier = True
-        # Arret de la boucle si arriver a la fin de l'arbre
-        if cleAEnvoyer == "0":
             print("\nThis is the end of the game\n")
             ok = False
-        s_comm.send(cleAEnvoyer.encode())
-
-    else:
-        print("\nThis is the end of the game\n")
-        ok = False
 
 # Fermeture de la socket
-try:
-    s_comm.close()
-    s.close()
-except OSError:
-    print('Socket encore ouverte !')
-else:
-    print('Socket correctement fermée')
+def fermetureSocket(s, s_comm):
+    try:
+        s_comm.close()
+        s.close()
+    except OSError:
+        print('Socket encore ouverte !')
+    else:
+        print('Socket correctement fermée')
+
+s = creationSocket()
+s_comm = accepter(s)
+programmePrincipal(s, s_comm)
+fermetureSocket(s, s_comm)
--- Joueur2/client_CyberExpert.py
+++ Joueur2/client_CyberExpert.py
@@ -11,85 +11,92 @@ with open(path + '\QA.json') as mon_fichier:
     data = json.load(mon_fichier)
 
 # Création de la socket du client
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Description socket : ', s)
-except OSError:
-    print('Création socket échouée')
-else:
-    print('Création socket réussie')
-    coord_S = ('127.0.0.1', 65432)
+def creationSocket():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except OSError:
+        print('Socket creation failed')
+    else:
+        print('Socket creation success')
+        coord_S = ('127.0.0.1', 65432)
+    return(s, coord_S)
 
 # Connection au serveur (Le DSI)
-s.connect(coord_S)
-print("Connection effectuée")
+def accepter(s, coord_S):
+    s.connect(coord_S)
+    print("connection established")
 
 cleDepart = "1"
 
-# Envoi de la première question au serveur (DSI)
-cleAEnvoyerAuServeur = cleDepart
-print(cleAEnvoyerAuServeur)
-s.send(cleAEnvoyerAuServeur.encode())
+def programmePrincipal(s):
+    # Envoi de la première question au serveur (DSI)
+    cleAEnvoyerAuServeur = cleDepart
+    s.send(cleAEnvoyerAuServeur.encode())
 
-ok = True
-choixAFaire = True
+    ok = True
+    choixAFaire = True
 
-# Explication du contexte du jeu
-print("\nYou are a cybersecurity expert, a new client calls you because his company has been hacked.\nThrough different questions you will have to understand how he got hacked and how you can help him.\nGood luck to you\n")
+    # Explication du contexte du jeu
+    print("\nYou are a cybersecurity expert, a new client calls you because his company has been hacked.\nThrough different questions you will have to understand how he got hacked and how you can help him.\nGood luck to you\n")
+    while ok :
+        question2 = True
+        reponseServeur = s.recv(1024).decode()
+        # Arret de la boucle si arriver a la fin de l'arbre sinon continue à communiquer
+        if reponseServeur == "0":
+            # Afficher la reponse envoyer par le serveur
+            if data[cleAEnvoyerAuServeur]["reponse1"]["cleNext"] == reponseServeur:
+                print(data[cleAEnvoyerAuServeur]["reponse1"]["reponse"])
+            else:
+                print(data[cleAEnvoyerAuServeur]["reponse2"]["reponse"])
+            print("\nThis is the end of the game\n")
+            ok = False
+        elif(cleAEnvoyerAuServeur != "0"):
+            print("\nquestion :")
+            # Afficher la reponse envoyer par le serveur
+            if data[cleAEnvoyerAuServeur]["reponse1"]["cleNext"] == reponseServeur:
+                print(data[cleAEnvoyerAuServeur]["reponse1"]["reponse"])
+            else:
+                print(data[cleAEnvoyerAuServeur]["reponse2"]["reponse"])
 
-while ok :
-    question2 = True
-    reponseServeur = s.recv(1024).decode()
-    # Arret de la boucle si arriver a la fin de l'arbre sinon continue à communiquer
-    if reponseServeur == "0":
-        # Afficher la reponse envoyer par le serveur
-        if data[cleAEnvoyerAuServeur]["reponse1"]["cleNext"] == reponseServeur:
-            print(data[cleAEnvoyerAuServeur]["reponse1"]["reponse"])
+            print("\n1)", data[reponseServeur]["question1"]["question"])
+            try:
+                print("2)", data[reponseServeur]["question2"]["question"] + "\n")
+            except:
+                print("")
+                question2 = False
+
+            choix = input("Choose between 1 and 2 : ")
+    
+            # Choix de la question à envoyer
+            if choix == "1":
+                cleAEnvoyerAuServeur = data[reponseServeur]["question1"]["cleNext"]
+            elif choix == "2" and question2 == True:
+                cleAEnvoyerAuServeur = data[reponseServeur]["question2"]["cleNext"]
+            else:
+                # Boucle qui gère l'erreur (si il appuye sur autre que que 1 ou 2 redemande)
+                while(choix != "1" and choix !="2" or question2 == False):
+                    choix = input("Choose between 1 and 2 : ")       
+                    if choix == "1":
+                        cleAEnvoyerAuServeur = data[reponseServeur]["question1"]["cleNext"]
+                        question2 = True
+                    elif choix == "2" and question2 == True:
+                        cleAEnvoyerAuServeur = data[reponseServeur]["question2"]["cleNext"]
+
+            s.send(cleAEnvoyerAuServeur.encode())
         else:
-            print(data[cleAEnvoyerAuServeur]["reponse2"]["reponse"])
-        print("\nThis is the end of the game\n")
-        ok = False
-    elif(cleAEnvoyerAuServeur != "0"):
-        print("\nquestion :")
-        # Afficher la reponse envoyer par le serveur
-        if data[cleAEnvoyerAuServeur]["reponse1"]["cleNext"] == reponseServeur:
-            print(data[cleAEnvoyerAuServeur]["reponse1"]["reponse"])
-        else:
-            print(data[cleAEnvoyerAuServeur]["reponse2"]["reponse"])
+            print("\nThis is the end of the game\n")
+            ok = False
 
-        print("\n1)", data[reponseServeur]["question1"]["question"])
-        try:
-            print("2)", data[reponseServeur]["question2"]["question"])
-        except:
-            print("")
-            question2 = False
-
-        choix = input("Choose between 1 and 2 : ")
- 
-        # Choix de la question à envoyer
-        if choix == "1":
-            cleAEnvoyerAuServeur = data[reponseServeur]["question1"]["cleNext"]
-        elif choix == "2" and question2 == True:
-            cleAEnvoyerAuServeur = data[reponseServeur]["question2"]["cleNext"]
-        else:
-            # Boucle qui gère l'erreur (si il appuye sur autre que que 1 ou 2 redemande)
-            while(choix != "1" and choix !="2" or question2 == False):
-                choix = input("Choose between 1 and 2 : ")       
-                if choix == "1":
-                    cleAEnvoyerAuServeur = data[reponseServeur]["question1"]["cleNext"]
-                    question2 = True
-                elif choix == "2" and question2 == True:
-                    cleAEnvoyerAuServeur = data[reponseServeur]["question2"]["cleNext"]
-
-        s.send(cleAEnvoyerAuServeur.encode())
+# Fermeture de la socket  
+def fermetureSocket(s):  
+    try:
+        s.close()
+    except OSError:
+        print('Socket encore ouverte !')
     else:
-        print("\nThis is the end of the game\n")
-        ok = False
+        print('Socket correctement fermée')
 
-# Fermeture de la socket    
-try:
-    s.close()
-except OSError:
-    print('Socket encore ouverte !')
-else:
-    print('Socket correctement fermée')
+s, coord_S = creationSocket()
+accepter(s, coord_S)
+programmePrincipal(s)
+fermetureSocket(s)
